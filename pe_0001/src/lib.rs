80232a469c21ca27879aa782612cbf1c94d9b57c
Alex Myodov <amyodov@gmail.com>
2023-10-10 14:53:56 +0100

    Proper multi-package/multi-library Cargo workspace structure.

--- pe_0001/src/lib.rs
+++ pe_0001/src/lib.rs
@@ -5,3 +5,14 @@ fn items() -> impl Iterator<Item=i64> {
 pub fn calc() -> i64 {
     items().sum()
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_result() {
+        assert_eq!(calc(), 233168);
+    }
+}
--- src/main.rs
+++
@@ -1,6 +0,0 @@
-mod pe_0001;
-mod pe_0005;
-
-fn main() {
-    println!("Result: {}", pe_0001::calc());
-}
