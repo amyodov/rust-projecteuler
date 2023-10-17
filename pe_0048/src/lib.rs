13057bb126c65101a19c57fa6dc509def3266c5b
Alex Myodov <amyodov@gmail.com>
2023-10-17 20:11:57 +0100

    Task 48 solution.

---
+++ pe_0048/src/lib.rs
@@ -0,0 +1,40 @@
+use num_bigint::BigUint;
+use std::str::FromStr;
+
+fn last_digits(n: BigUint, n_digits: u8) -> u64 {
+    let pow_of_10 = BigUint::from(10u8).pow(n_digits as u32);
+    (n % pow_of_10).try_into().unwrap()
+}
+
+fn sum_facts(limit: usize) -> BigUint {
+    (1..=limit)
+        .map(|i| BigUint::from(i).pow(i as u32))
+        .sum()
+}
+
+pub fn calc() -> u64 {
+    last_digits(sum_facts(1000), 10)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_last_digits() {
+        assert_eq!(last_digits(BigUint::from_str("1234567890").unwrap(), 3),
+                   890);
+        assert_eq!(last_digits(BigUint::from_str("12345678901234567890").unwrap(), 10),
+                   1234567890);
+    }
+
+    #[test]
+    fn test_sum_facts() {
+        assert_eq!(sum_facts(10), BigUint::from(10405071317u64));
+    }
+
+    #[test]
+    fn test_calc() {
+        assert_eq!(calc(), 9110846700);
+    }
+}
