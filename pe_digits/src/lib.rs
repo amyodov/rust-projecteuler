c52ef1a699b9e005d20d2106ca36d24081ae3f1a
Alex Myodov <amyodov@gmail.com>
2023-10-18 01:28:31 +0100

    pe_digits is now generic, and supports both u64/u128 and BigUint.

--- pe_digits/src/lib.rs
+++ pe_digits/src/lib.rs
@@ -1,12 +1,24 @@
-pub fn digits_of_number(n: u64) -> impl Iterator<Item=u8> {
-    let mut n_op = n;
-    let est_digits = n.ilog10() + 1;
-    let mut result: Vec<u8> = Vec::with_capacity(est_digits as usize);
+use std::ops::{Div, Rem};
+use num::{FromPrimitive, Integer, ToPrimitive};
 
-    while n_op > 0 {
-        let digit = n_op % 10;
-        n_op = n_op / 10;
-        result.push(digit as u8);
+/// Make an Iterator with the digits of number.
+///
+/// Generic function; supports both `u64`/`u128` and `BigUint`.
+pub fn digits_of_number<T>(n: T) -> impl Iterator<Item=u8>
+    where T: Rem<T, Output=T> + Div<T, Output=T> + FromPrimitive + ToPrimitive
+    + Integer
+{
+    let zero: T = FromPrimitive::from_u64(0).unwrap();
+    let ten: T = FromPrimitive::from_u64(10).unwrap();
+
+    let mut n: T = n;
+    let est_digits = 10;
+    let mut result: Vec<u8> = Vec::with_capacity(est_digits as usize);
+    let mut digit: T;
+
+    while n > zero {
+        (n, digit) = n.div_rem(&ten);
+        result.push(digit.to_u8().unwrap());
     }
 
     result.into_iter().rev()
@@ -16,11 +28,20 @@ pub fn digits_of_number(n: u64) -> impl Iterator<Item=u8> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use num::bigint::BigUint;
     use pe_itertools::iterator_as_list;
 
     #[test]
     fn test_digits_of_number() {
-        assert_eq!(iterator_as_list(digits_of_number(239487239842234)),
+        assert_eq!(iterator_as_list(digits_of_number(23948723)),
+                   "2,3,9,4,8,7,2,3");
+        assert_eq!(iterator_as_list(digits_of_number(239487239842234u64)),
+                   "2,3,9,4,8,7,2,3,9,8,4,2,2,3,4");
+        assert_eq!(iterator_as_list(digits_of_number(239487239842234u128)),
+                   "2,3,9,4,8,7,2,3,9,8,4,2,2,3,4");
+
+        let b: BigUint = BigUint::from(239487239842234u64);
+        assert_eq!(iterator_as_list(digits_of_number(b)),
                    "2,3,9,4,8,7,2,3,9,8,4,2,2,3,4");
     }
 }
